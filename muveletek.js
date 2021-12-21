e220edbb4f3ad69ad793c3f919b52d4c09180fb1
Gábor Polgár <56192725+gaborpolgar@users.noreply.github.com>
2021-12-21 22:15:54 +0100

    kivon szoroz oszt functions are done.

---
+++ muveletek.js
@@ -0,0 +1,20 @@
+function osszead(a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    return a + b;
+}
+function kivon(c, d){
+    c = parseFloat(c);
+    d = parseFloat(d);
+    return c - d;
+}
+function szoroz(e, f){
+    e=parseFloat(e);
+    f=parseFloat(f);
+    return e*f;
+}
+function oszt(g, h){
+    g=parseFloat(g);
+    h=parseFloat(h);
+    return g/h;
+}
